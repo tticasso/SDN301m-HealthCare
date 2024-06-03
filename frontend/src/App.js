44205f3ed6c50af70ae90434fee872d708620e85
tticasso <takhanhtung31@gmail.com>
2024-06-04 02:02:54 +0700

    HC-6 Login Signup UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import RegisterForm from "./fake-data";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+
 function App() {
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center items-center">
