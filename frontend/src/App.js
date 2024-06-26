562e930bbcb050c102e2bb89d319ab135b5f16ee
LaHieu11 <lahieutx@gmail.com>
2024-06-26 21:19:48 +0700

    HC-52

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import UserProfileMenu from "./pages/UserProfileMenu";
 import Signup from "./components/Signup";
+import Login from "./components/Login";
 import Homepage from "./pages/Homepage";
 import DoctorList from "./pages/DoctorList";
 import BookingAppointment from "./pages/BookingAppointment";
@@ -12,6 +13,7 @@ const App = () => {
     <div className="w-screen h-screen flex justify-center items-center">
       <Routes>
         <Route path="/signup" element={<Signup/>}/>
+        <Route path="/login" element={<Login/>}/>
         <Route path="/menu" element={<UserProfileMenu />} />
         <Route path="/booking/:id" element={<BookingAppointment />} />
         <Route path="/" element={<Homepage />} />
