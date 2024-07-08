de16b8869afa22eeda9fb3b65cdc959278394bab
conghieu281 <conghieu8899.work@gmail.com>
2024-07-08 22:35:07 +0700

    fix bug

--- backend/src/services/Specify.services.js
+++ backend/src/services/Specify.services.js
@@ -1,11 +1,6 @@
 const Specify = require('../models/Specify.model')
 
 
-<<<<<<< HEAD
-
-=======
-//tạo 1 schedule
->>>>>>> b8d1927eddd35b33b6a4c0348d27f0d0c99fdc24
 const createSpecify = async (specify) => {
     if (await Specify.findOne({name: specify.name})) {
         throw new Error('Chuyên ngành việc đã tồn tại');
@@ -15,11 +10,6 @@ const createSpecify = async (specify) => {
     return newSpecify;
 };
 
-<<<<<<< HEAD
-
-=======
-// Lấy 1 profile
->>>>>>> b8d1927eddd35b33b6a4c0348d27f0d0c99fdc24
 const getSpecify = async (id) => {
     const specify = await Specify.findById(id);
     if (!specify) {
@@ -28,13 +18,8 @@ const getSpecify = async (id) => {
     return specify;
 };
 
-<<<<<<< HEAD
 
 const updateSpecify = async (id, data) => {
-=======
-// Cập nhật ca lam (nhan vien)
-const updateDocSchedule = async (id, data) => {
->>>>>>> b8d1927eddd35b33b6a4c0348d27f0d0c99fdc24
     const specify = await Specify.findByIdAndUpdate(id, data)
     if (!specify) {
         throw new Error('Chuyên ngành không tồn tại');
@@ -42,20 +27,14 @@ const updateDocSchedule = async (id, data) => {
     return specify;
 };
 
-<<<<<<< HEAD
 
 const deleteSpecify = async (id) => {
-=======
-// Xoa profile (admin)
-const deleteDocSchedule = async (id) => {
->>>>>>> b8d1927eddd35b33b6a4c0348d27f0d0c99fdc24
     const specify = await Specify.findByIdAndDelete(id);
     if (!specify) {
         throw new Error('Chuyêng ngành không tồn tại');
     }
 };
 
-<<<<<<< HEAD
 const getAllSpecify = async () => {
     const specifies = await Specify.find();
     if(!specifies) {
@@ -64,21 +43,12 @@ const getAllSpecify = async () => {
 }
 
 
-const docScheduleService = {
+const SpecifyService = {
     getSpecify,
     getAllSpecify,
     createSpecify,
     deleteSpecify,
     updateSpecify
-=======
-
-
-const docScheduleService = {
-    createDocSchedule,
-    getDocSchedule,
-    deleteDocSchedule,
-    updateDocSchedule,
->>>>>>> b8d1927eddd35b33b6a4c0348d27f0d0c99fdc24
 }
 
-module.exports = docScheduleService;
+module.exports = SpecifyService;
